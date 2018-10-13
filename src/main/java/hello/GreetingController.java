2ecec0a298ec250fe7cfc24ce2437656ea220dfc
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2018-10-13 22:14:50 +0100

    die hello die

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -33,7 +33,7 @@ public class GreetingController {
     @SendTo("/topic/greetings")
       public Greeting greeting(HelloMessage message) throws Exception {
 
-        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+        return new Greeting(HtmlUtils.htmlEscape(message.getName()));
 	
 //	bbb.doStuff(message.getName());
 	}
