790cc80afd83f5e70a536fe2a113d3f34b19cb7c
Tello-Wharton <aaron.tkd@hotmail.com>
2018-10-13 21:09:14 +0100

    Hopefully tell nexmo how to handle the call

--- src/main/java/hello/CallController.java
+++ src/main/java/hello/CallController.java
@@ -1,8 +1,13 @@
 package hello;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nexmo.client.voice.ncco.Ncco;
+import com.nexmo.client.voice.ncco.TalkNcco;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.util.HtmlUtils;
@@ -17,8 +22,18 @@ public class CallController {
 //    public String greeting(HelloMessage message) {
 //        return message.toString();
 //    }
-    @PostMapping("/call")
-    public void call(@RequestBody Map<String, Object> payload) {
+    @PostMapping("/event")
+    public String event(@RequestBody Map<String, Object> payload) {
         System.out.print(payload);
+        return "";
+    }
+
+    @GetMapping(value = "/answer", produces = "application/json")
+    public String answer() throws JsonProcessingException {
+        TalkNcco message = new TalkNcco("Hey, it's callum... Yes? Oh yeah! mhm? Just kidding, it's an answer machine beeeep");
+
+        Ncco[] nccos = new Ncco[]{message};
+
+        return new ObjectMapper().writer().writeValueAsString(nccos);
     }
 }
