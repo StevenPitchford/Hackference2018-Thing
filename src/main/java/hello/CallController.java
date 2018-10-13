b5cf20ba9da15d211245f4a433a5ce573fe91644
Tello-Wharton <aaron.tkd@hotmail.com>
2018-10-13 22:06:01 +0100

    Actually bind answer

--- src/main/java/hello/CallController.java
+++ src/main/java/hello/CallController.java
@@ -4,13 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nexmo.client.voice.ncco.Ncco;
 import com.nexmo.client.voice.ncco.TalkNcco;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.util.HtmlUtils;
 
 import java.util.Map;
 
@@ -28,7 +25,7 @@ public class CallController {
         return "";
     }
 
-    @GetMapping(value = "/answer", produces = "application/json")
+    @GetMapping(path = "/answer", produces = "application/json")
     public String answer() throws JsonProcessingException {
         TalkNcco message = new TalkNcco("Hey, it's callum... Yes? Oh yeah! mhm? Just kidding, it's an answer machine beeeep");
 
