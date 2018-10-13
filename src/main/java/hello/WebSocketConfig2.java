8a7668d780cf53407cd1c77f782492d673fdb84f
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2018-10-13 23:48:03 +0100

    forgot annotations

--- src/main/java/hello/WebSocketConfig2.java
+++ src/main/java/hello/WebSocketConfig2.java
@@ -1,10 +1,14 @@
 package hello;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
+@Configuration
+@EnableWebSocket
 public class WebSocketConfig2 implements WebSocketConfigurer {
 
     @Bean
